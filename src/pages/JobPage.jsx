db0b8367c2834cccd7f6376c471294e4abe6100d
bread dev <brynsnts1@gmail.com>
2025-03-06 21:52:51 +0800

    Update JobPage.jsx

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 const JobPage = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const { id } = useParams();
+  const [job, setJob] = useState(null);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const fetchJob = async () => {
+      try {
+        const res = await fetch("/api/jobs/${id}");
+        const data = await res.json();
+        setJob(data);
+      } catch (error) {
+        console.log("Error Fetching data", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchJob();
+  }, []);
+  return <div>Job Page</div>;
+};
 
-export default JobPage
+export default JobPage;
